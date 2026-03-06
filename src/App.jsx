9a765e1188ebb78c43ca990bea206d245ae7ee55
João Lucas <132568730+JOTINHALUCAS@users.noreply.github.com>
2026-03-06 10:57:04 -0300

    Foi feito alguns ajustes no Front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import Header from "./components/Header";
 import InputCdd from "./components/InputCdd";
-
+import wordloc from "./assets/icons/wordloc.jpg";
 function App() {
   return (
     <div>
       <Header />
       <InputCdd />
+
+      <div className="w-full">
+        <div className="max-w-md mx-auto w-max-2xl  flex flex-col items-center justify-center p-8 ">
+          <img src={wordloc} className="w-40 rounded-4xl p-4" />
+          <p className="font font-bold italic">
+            Descubra o clima da sua cidade
+          </p>
+          <p className="text-sm text-gray-500">
+            Busque por qualquer cidade do mundo e receba informações climaticas
+            em tempo real, junto com dicas de sustentabilidade geradas por IA
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/InputCdd.jsx
+++ src/components/InputCdd.jsx
@@ -1,6 +1,13 @@
 import { Search } from "lucide-react";
+import { useState } from "react";
 
 function InputCdd() {
+  const [cidade, setCidade] = useState("");
+
+  function handleBuscar() {
+    console.log(cidade);
+  }
+
   return (
     <div className="w-full">
       <div className="flex w-full max-w-md mx-auto w-max-4xl h-11 shadow focus:shadow-emerald-400 pointer rounded-md items-center justify-between mt-4 p-4 border-0">
@@ -9,19 +16,18 @@ function InputCdd() {
           type="text"
           placeholder="Digite uma cidade"
           className="p-6 b rounded-sm focus:right-2 focus:outline-none focus:ring-green-200 text-sm"
+          onChange={(e) => setCidade(e.target.value)}
         />
-        <div className="bg-emerald-300 rounded-2xl w-20 h-8 flex items-center justify-center shadow">
-          <button className=" text-sm text-amber-50 text-center h-6 ">
+
+        <div className="bg-emerald-300 rounded-2xl w-20 h-8 flex items-center justify-center shadow ">
+          <button
+            className=" text-sm text-amber-50 text-center w-20 h-8 rounded-2xl focus:ring-2 focus:ring-emerald-300 shadow"
+            value={cidade}
+            onClick={handleBuscar}
+          >
             Buscar
           </button>
         </div>
-      </div>
-      <div>
-        <p>Descubra o clima da sua cidade</p>
-        <p>
-          Busque por qualquer cidade do mundo e receba informações climaticas em
-          tempo real, junto com dicas de sustentabilidade geredas por IA
-        </p>
       </div>
     </div>
   );
